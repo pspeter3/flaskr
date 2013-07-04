fd98d4658c83864028d182d6272a861990af2e1a
Phips Peter <pspeter333@gmail.com>
2013-07-03 19:56:47 -0700

    Adding the database bootstrap

--- flaskr.py
+++ flaskr.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 import sqlite3
+from contextlib import closing
 from flask import Flask, request, session, g, redirect, url_for, abort, \
   render_template, flash
 
@@ -16,6 +17,12 @@ app.config.from_envvar('FLASKR_SETTINGS', silent=True)
 
 def connect_db():
   return sqlite3.connect(app.config['DATABASE'])
-  
+
+def init_db():
+    with closing(connect_db()) as db:
+        with app.open_resource('schema.sql', mode='r') as f:
+            db.cursor().executescript(f.read())
+        db.commit()
+
 if __name__ == '__main__':
   app.run()
