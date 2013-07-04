42b3aaf4387759ed0502a6482c2e5199054b8e44
Phips Peter <pspeter333@gmail.com>
2013-07-03 20:09:40 -0700

    Added authentication

--- flaskr.py
+++ flaskr.py
@@ -50,5 +50,24 @@ def create():
   flash('Created entry')
   return redirect(url_for('index'))
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+  error = None
+  if request.method == 'POST':
+    if request.form['username'] != app.config(['USERNAME'])
+      error = 'Invalid username'
+    elif request.form['password'] != app.config(['PASSWORD'])
+      error = 'Invalid password'
+    else:
+      session['logged_in'] = True
+      flash('Successfully logged in')
+      return redirect(url_for('index'))
+  return render_template('login.html', error=error)
+
+@app.route('/logout')
+def logout():
+  session.pop('logged_in', None)
+  flash('Successfully logged out')
+  return redirect(url_for('index'))
 if __name__ == '__main__':
   app.run()
